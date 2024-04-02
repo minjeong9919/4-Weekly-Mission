90b8a175093c69e38c265f88e9b21d7f7a5f4a9a
Kim Min Jeong <minjeong9919@naver.com>
2024-04-02 12:19:11 +0900

    Refactor: api 공통 부분 함수로 묶어 분리

--- api/api.ts
+++ api/api.ts
@@ -1,18 +1,8 @@
 const BASE_URL = "https://bootcamp-api.codeit.kr/api/";
 
-export const getFolderInfo = async () => {
+const fetchJson = async (url: string) => {
   try {
-    const response = await fetch(`${BASE_URL}sample/folder`);
-    const result = await response.json(); // 재사용성을 위해 response.json()으로 끝맺는게 좋음
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const getUserInfo = async () => {
-  try {
-    const response = await fetch(`${BASE_URL}sample/user`);
+    const response = await fetch(url);
     const result = await response.json();
     return result;
   } catch (error) {
@@ -20,25 +10,23 @@ export const getUserInfo = async () => {
   }
 };
 
+export const getFolderInfo = async () => {
+  const url = `${BASE_URL}sample/folder`;
+  return await fetchJson(url);
+};
+
+export const getUserInfo = async () => {
+  const url = `${BASE_URL}sample/user`;
+  return await fetchJson(url);
+};
+
 export const getFolderList = async () => {
-  try {
-    const response = await fetch(`${BASE_URL}users/1/folders`);
-    const result = await response.json(); // 재사용성을 위해 response.json()으로 끝맺는게 좋음
-    // console.log(result);
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
+  const url = `${BASE_URL}users/1/folders`;
+  return fetchJson(url);
 };
 export const getAllLinkData = async (id: string) => {
   const url = id
     ? `${BASE_URL}users/1/folders/${id}`
     : `${BASE_URL}users/1/links`;
-  try {
-    const response = await fetch(url);
-    const result = await response.json(); // 재사용성을 위해 response.json()으로 끝맺는게 좋음
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
+  return fetchJson(url);
 };
