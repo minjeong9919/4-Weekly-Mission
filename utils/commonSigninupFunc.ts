515b2d648db618fa306071f73e9e61c28ae0442c
Kim Min Jeong <minjeong9919@naver.com>
2024-04-06 15:33:26 +0900

    Comment: 주석 제거

--- utils/commonSigninupFunc.ts
+++ utils/commonSigninupFunc.ts
@@ -1,6 +1,3 @@
-import { ERROR_MESSAGE } from "@/constants/errorMessage";
-// import { pwdInput, pwdEyeIcon } from "../declaration.js";
-
 export const changeInputBorderColor = (status: string | undefined) => {
   switch (status) {
     case "default":
@@ -11,68 +8,3 @@ export const changeInputBorderColor = (status: string | undefined) => {
       return "var(--Red)";
   }
 };
-
-// 에러 메세지 출력 함수
-// export function errorMsg(errorCase: string) {
-//   let newNode = document.createElement("div");
-
-//   switch (errorCase) {
-//     case "NoEmail":
-//       inputAddNode({
-//         type: "email",
-//         message: ERROR_MESSAGE.email.empty,
-//         errorCase: errorCase,
-//       });
-//       break;
-//     case "wrongEmail":
-//       inputAddNode({
-//         type: "email",
-//         message: ERROR_MESSAGE.email.invalid,
-//         errorCase: errorCase,
-//       });
-//       break;
-//     case "inUseEmail":
-//       inputAddNode({
-//         type: "email",
-//         message: ERROR_MESSAGE.email.inUse,
-//         errorCase: errorCase,
-//       });
-//       break;
-//     case "NoPwd":
-//       inputAddNode({
-//         type: "password",
-//         message: ERROR_MESSAGE.password.empty,
-//         errorCase: errorCase,
-//       });
-//       break;
-//     case "wrongPwd":
-//       inputAddNode({
-//         type: "password",
-//         message: ERROR_MESSAGE.password.invalid,
-//         errorCase: errorCase,
-//       });
-//       break;
-//     case "noMatchPwd":
-//       inputAddNode({
-//         type: "password2",
-//         message: ERROR_MESSAGE.password.recheck,
-//         errorCase: errorCase,
-//       });
-//       break;
-//     case "Other":
-//       inputAddNode({
-//         type: "email",
-//         message: ERROR_MESSAGE.email.check,
-//         errorCase: errorCase,
-//       });
-//       inputAddNode({
-//         type: "password",
-//         message: ERROR_MESSAGE.password.check,
-//         errorCase: errorCase,
-//       });
-//       break;
-//   }
-// }
-
-// // password input 눈모양 클릭 실행 함수
-// export function EyePwd(EyeStatus: string) {}
